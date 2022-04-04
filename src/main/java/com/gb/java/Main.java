bc398dc60fed5a265fcbeffa4326a0655b67020c
Andrei <93673737+andreidfr0@users.noreply.github.com>
2022-04-04 22:52:02 +0300

    first commit

---
+++ src/main/java/com/gb/java/Main.java
@@ -0,0 +1,12 @@
+package com.gb.java;
+public class Main {
+
+    /*1. Создать три потока, каждый из которых выводит
+    определенную букву (A, B и C) 5 раз (порядок – ABСABСABС).
+     Используйте wait/notify/notifyAll.
+    2. На серверной стороне сетевого чата реализовать
+    управление потоками через ExecutorService.*/
+    public static void main(String[] args) {
+
+    }
+}
